b9eb0d373196bdebbc39faf4e73ccb3a19e15c9d
alan <alanduarte90@gmail.com>
2018-03-23 17:50:28 -0300

    Start adding cool effects on box-shadow and remove comment lines

--- js/app.js
+++ js/app.js
@@ -4,13 +4,10 @@ addBoxShadow();
 function addBoxShadow(){
     let select = document.querySelectorAll(".styles");
     select.forEach(function(currentVal, index, lisObjs){
-        // console.log(currentVal +" "+ index +" "+ lisObjs);
         currentVal.addEventListener("mouseover", function() {
-          //alert(event);
-          currentVal.style.boxShadow = "10px 10px 5px #d3d3d3";
+          currentVal.style.boxShadow = "0 0 0 3px hsl(0, 0%, 30%), 0 0 0 7px hsl(0, 0%, 50%), 0 0 0 12px hsl(0, 0%, 70%)";
         });
         currentVal.addEventListener("mouseout", function() {
-          //alert(event);
           currentVal.style.boxShadow = "0px 0px 0px #000";
         });
     });
