35de42004f8aba544ca9a06a417b0b2a86c635eb
alan <alanduarte90@gmail.com>
2018-03-23 16:37:52 -0300

    Add initial prototype of addBoxShadow function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 
+addBoxShadow();
+
+function addBoxShadow(){
+    let select = document.querySelectorAll(".styles");
+    select.forEach(function(currentVal, index, lisObjs){
+        // console.log(currentVal +" "+ index +" "+ lisObjs);
+        currentVal.addEventListener("mouseover", function() {
+          //alert(event);
+          currentVal.style.boxShadow = "10px 10px 5px #d3d3d3";
+        });
+        currentVal.addEventListener("mouseout", function() {
+          //alert(event);
+          currentVal.style.boxShadow = "0px 0px 0px #000";
+        });
+    });
+}
