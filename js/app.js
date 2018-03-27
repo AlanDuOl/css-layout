b69150e2c46166ec2cece44f6cce8574442df5d0
alan <alanduarte90@gmail.com>
2018-03-27 11:35:18 -0300

    Start of layout redefinition

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ function addBoxShadow(){
     let select = document.querySelectorAll(".box");
     select.forEach(function(currentVal, index, lisObjs){
         currentVal.addEventListener("mouseover", function() {
-          currentVal.style.boxShadow = "0 0 0 3px hsl(0, 0%, 30%), 0 0 0 7px hsl(0, 0%, 50%), 0 0 0 12px hsl(0, 0%, 70%)";
+          currentVal.style.boxShadow = "1px 1px 0 3px hsl(0, 0%, 90%), 1px 1px 0 6px hsl(0, 0%, 60%), 1px 1px 0 10px hsl(0, 0%, 30%)";
         });
         currentVal.addEventListener("mouseout", function() {
           currentVal.style.boxShadow = "0px 0px 0px #000";
