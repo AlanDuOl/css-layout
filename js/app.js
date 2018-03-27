f5e9dcd4a8784eb7ef7f0354d3695827108b26ca
alan <alanduarte90@gmail.com>
2018-03-27 10:24:50 -0300

    Change box-shadow from main div to inside divs and fix some format issues

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 addBoxShadow();
 
 function addBoxShadow(){
-    let select = document.querySelectorAll(".styles");
+    let select = document.querySelectorAll(".box");
     select.forEach(function(currentVal, index, lisObjs){
         currentVal.addEventListener("mouseover", function() {
           currentVal.style.boxShadow = "0 0 0 3px hsl(0, 0%, 30%), 0 0 0 7px hsl(0, 0%, 50%), 0 0 0 12px hsl(0, 0%, 70%)";
