aca897478d63ad1eb8b9ef2e3ae2f0520358657a
alan <alanduarte90@gmail.com>
2018-04-05 14:20:46 -0300

    Change function addBoxShadow to mouseoverEffect. -Add mouse effect

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-addBoxShadow();
+mouseoverEffect();
 layoutChoice();
 
 function initialLoad(){
@@ -46,11 +46,12 @@ function layoutChoice(){
 }
 
 //Add/remove boxShadow of elements on mousever event
-function addBoxShadow(){
+function mouseoverEffect(){
     let select = document.querySelectorAll(".box");
     select.forEach(function(currentVal, index, lisObjs){
         currentVal.addEventListener("mouseover", function() {
           currentVal.style.boxShadow = "1px 1px 0 3px hsl(0, 0%, 90%), 1px 1px 0 6px hsl(0, 0%, 60%), 1px 1px 0 10px hsl(0, 0%, 30%)";
+          currentVal.style.cursor = "pointer";
         });
         currentVal.addEventListener("mouseout", function() {
           currentVal.style.boxShadow = "0px 0px 0px #000";
